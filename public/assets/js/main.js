48ffca0837c977685c6d51de64f5d5a21d16b1b3
Shazil Tovar <shazil@gmail.com>
2017-07-18 22:47:42 -0500

    avance hasta funcion de validacion

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,6 +1,42 @@
 $(document).ready(function(){
       $('.carousel').carousel();
+     
     });
+
+  var $inputRegistro = $(".inputRegistro");
+     var $btnRegistro = $("#btnRegistro");
+     var $form = $("#registro-form");
+     var $check = $("#checkBoxRegistro");
+
+     console.log($check);
+
+     $form.submit(sacarNumero)
+     $inputRegistro.keyup(contador)
+
+ 
+
+ function contador() {
+     var contador = $inputRegistro.val().length
+     if (contador == 10) {
+         $btnRegistro.removeAttr("disabled")
+         
+     } else {
+         $btnRegistro.attr("disabled", "true")
+         $check.removeAttr("checked")
+     }
+     
+   
+     
+     if($check.check){
+         console.log("hola");
+     }
+
+ }
+
+ function sacarNumero(e) {
+     e.preventDefault();
+     console.log($inputRegistro.val())
+ }
 
  
 
